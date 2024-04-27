3b25c2665254598a0aa0c84c70c04c4d079ff9c4
mrkyaing <akog02@gmail.com>
2024-04-27 15:45:18 +0700

    HRMS web 2

--- OOPPractice/obj/Debug/net6.0/OOPPractice.AssemblyInfo.cs
+++ OOPPractice/obj/Debug/net6.0/OOPPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aedba00c94775e04ad9fd306ca289eecae516b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf45fc2c60644164bbd395fe05fb0b5b70dead1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
